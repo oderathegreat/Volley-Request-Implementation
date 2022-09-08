a7b5cca55f82e55fc13b8a645973a379a5a0b6f0
oderathegreat <oderathegreat@gmail.com>
2022-09-08 11:35:30 +0300

    volley implementation

--- app/src/main/java/com/example/volley_api/MainActivity.kt
+++ app/src/main/java/com/example/volley_api/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.volley_api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
 
     val volleyRequest: RequestQueue? = null
+    val url_link = "https://jsonplaceholder.typicode.com/users"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,28 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    fun fetchStringData(url:String) {
+        val stringReq = StringRequest(Request.Method.GET,url, {
+                response:String? ->
+            try {
+                 Log.d("Response ", response.toString())
+            } catch (e:JSONException) {
+                e.printStackTrace()
+            }
+
+        },
+            {
+                 //error listener
+                error:VolleyError? ->
+                try {
+                        Log.d("Error", error.toString())
+
+                } catch (e:JSONException) {
+                      e.printStackTrace()
+
+                }
+            })
     }
 }
