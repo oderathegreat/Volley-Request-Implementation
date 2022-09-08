d834fa41410007b410eaff88601ca59d293c0169
oderathegreat <oderathegreat@gmail.com>
2022-09-08 10:37:07 +0300

    initialised volley request

--- app/src/main/java/com/example/volley_api/MainActivity.kt
+++ app/src/main/java/com/example/volley_api/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.volley_api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.RequestQueue
 
 class MainActivity : AppCompatActivity() {
+
+    val volleyRequest: RequestQueue? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
