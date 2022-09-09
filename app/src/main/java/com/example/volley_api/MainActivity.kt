81954c60551c4fa588353b8dc4c2e2e07d31f379
oderathegreat <oderathegreat@gmail.com>
2022-09-09 12:04:07 +0300

    Instantiate our volley request

--- app/src/main/java/com/example/volley_api/MainActivity.kt
+++ app/src/main/java/com/example/volley_api/MainActivity.kt
@@ -8,6 +8,7 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Instantiate our volley request
+        volleyRequest = Volley.newRequestQueue(this)
+
         //calling our method
         fetchStringData(url_link)
 
