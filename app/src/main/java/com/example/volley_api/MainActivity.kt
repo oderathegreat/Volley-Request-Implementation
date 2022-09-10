df4f1d5b4894c678db119707998907975e7dc436
oderathegreat <oderathegreat@gmail.com>
2022-09-10 16:18:54 +0300

    implemented json array request plus fixed host unknown issue by switching my wifi on and off | added uses cleartextTraffic to true in xml manifest file

--- app/src/main/java/com/example/volley_api/MainActivity.kt
+++ app/src/main/java/com/example/volley_api/MainActivity.kt
@@ -7,14 +7,17 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
 
     var volleyRequest: RequestQueue? = null
     val url_link = "https://jsonplaceholder.typicode.com/users"
+    val movie_array_url = "http://api.tvmaze.com/search/shows?q=golden%20girls"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +27,48 @@ class MainActivity : AppCompatActivity() {
         volleyRequest = Volley.newRequestQueue(this)
 
         //calling our method
-        fetchStringData(url_link)
+        //fetchStringData(url_link)
+        fetchJsonArray(movie_array_url)
 
     }
+
+    fun fetchJsonArray(Url:String) {
+
+        val arrayReq = JsonArrayRequest(Request.Method.GET, Url,null,
+            {
+                    response: JSONArray ->
+                try {
+                    Log.d("Response-->" , response.toString())
+
+                } catch (e:JSONException) {
+
+                    e.printStackTrace()
+
+                }
+            },
+            {
+                    error:VolleyError? ->
+                try {
+
+                    Log.d("Error-->" , error.toString())
+
+                } catch (e:JSONException) {
+                    e.printStackTrace()
+
+                }
+
+            })
+
+        volleyRequest!!.add(arrayReq)
+
+
+
+
+    }
+
+
+
+
 
     fun fetchStringData(url:String) {
         val stringReq = StringRequest(Request.Method.GET,url, {
